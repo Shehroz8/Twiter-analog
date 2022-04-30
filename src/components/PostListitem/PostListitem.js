dddd0d76e9cce7302c7eff8d558b23b801cf47b7
Shehroz Raxmatov <98930700+Shehroz8@users.noreply.github.com>
2022-04-30 22:22:43 +0500

    Update PostListitem.js

--- src/components/PostListitem/PostListitem.js
+++ src/components/PostListitem/PostListitem.js
@@ -2,28 +2,11 @@ import React from "react";
 import "./PostListitem.css"
 
 export default class PostListitem extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            important: false,
-            like: false
-        }
-        this.onImportant = this.onImportant.bind(this)
-        this.onLike = this.onLike.bind(this)
-    }
 
-    onImportant(){
-        this.setState(({important}) => ({important: !important}))
-    }
-
-    onLike(){
-        this.setState(({like}) => ({like: !like}))
-    }
 
     render() {
 
-        const {label, onDelete, onToggleImportant, onToggleLiked} = this.props
-        const {important, like} = this.state
+        const {label, onDelete, onToggleImportant, onToggleLiked, important, like} = this.props
         let classNames = "app-list-item d-flex justify-content-between"
         if (important){
             classNames += ' important'
@@ -36,11 +19,11 @@ export default class PostListitem extends React.Component{
         return(
             <ul className="app-list list-group">
                 <li className={classNames}>
-                <span className="app-list-item-label" onClick={this.onLike}>
+                <span className="app-list-item-label" onClick={onToggleLiked}>
                     {label}
                 </span>
                     <div className="d-flex justify-content-center aligin-items-center">
-                        <button type="button" className="btn-star btn-sm" onClick={this.onImportant}>
+                        <button type="button" className="btn-star btn-sm" onClick={onToggleImportant}>
                             <i className="fa fa-star"></i>
                         </button>
                         <button  type="button" onClick={onDelete} className="btn-trash btn-sm">
